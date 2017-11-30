29bde5cedf027e0c1c8e8bb6660fdc99ee828fbf
Valerii Kuzin <kuzin.valeriy@gmail.com>
2017-11-30 21:24:16 +0200

    Added Registration User

--- src/app/auth/registration/registration.component.ts
+++ src/app/auth/registration/registration.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+import { UsersService } from '../../shared/services/users.service';
+import { User} from '../../shared/models/user.model';
+
 
 @Component({
   selector: 'hb-registration',
@@ -7,9 +12,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(
+    private userService: UsersService
+  ) { }
 
   ngOnInit() {
+    this.form = new FormGroup({
+      'email': new FormControl(null, [Validators.required, Validators.email ]),
+      'password': new FormControl(null, [Validators.required, Validators.minLength(5), Validators.maxLength(20)]),
+      'name': new FormControl(null, [Validators.required]),
+      'agree': new FormControl(false, [Validators.requiredTrue])
+    });
+  }
+
+  onSubmit() {
+
+    const { email, password, name } = this.form.value;
+    const user = new User(email, password, name);
+
+    this.userService.createNewUser(user)
+      .subscribe((user: User) => {
+        console.log(user);
+      });
   }
 
 }
