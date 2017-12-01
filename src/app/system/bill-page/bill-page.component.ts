23993e8012e1ca5908c36b3f67660e717d0774ea
Valerii Kuzin <kuzin.valeriy@gmail.com>
2017-12-01 21:11:10 +0200

    Added onRefresh method for BillPage

--- src/app/system/bill-page/bill-page.component.ts
+++ src/app/system/bill-page/bill-page.component.ts
@@ -13,21 +13,34 @@ import { Bill } from '../shared/models/bill.model';
 })
 export class BillPageComponent implements OnInit, OnDestroy {
 
-  subscription: Subscription;
+  sub1: Subscription;
+  sub2: Subscription;
+
+  currency: any;
+  bill: Bill;
 
   constructor(private billService: BillService) { }
 
   ngOnInit() {
-    this.subscription = Observable.combineLatest(
+    this.sub1 = Observable.combineLatest(
       this.billService.getBill(),
       this.billService.getCurrency()
     ).subscribe((data: [Bill, any]) => {
-      console.log(data);
+      this.bill = data[0];
+      this.currency = data[1];
     }) ;
   }
 
+  onRefresh() {
+    this.sub2 = this.billService.getCurrency()
+      .subscribe((currency: any) => {
+        this.currency = currency;
+      });
+  }
+
   ngOnDestroy() {
-    this.subscription.unsubscribe();
+    this.sub1.unsubscribe();
+    this.sub2.unsubscribe();
   }
 
 }
