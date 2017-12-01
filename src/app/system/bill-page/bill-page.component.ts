b00c917bd4164a5b05c05a7e1b715b91dbd24e6e
Valerii Kuzin <kuzin.valeriy@gmail.com>
2017-12-01 20:04:40 +0200

    Added BillServices for get bill^s data

--- src/app/system/bill-page/bill-page.component.ts
+++ src/app/system/bill-page/bill-page.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Subscription } from 'rxjs/Subscription';
+
+import { BillService } from '../shared/services/bill.service';
+import { Bill } from '../shared/models/bill.model';
+
+
 
 @Component({
   selector: 'hb-bill-page',
   templateUrl: './bill-page.component.html',
   styleUrls: ['./bill-page.component.scss']
 })
-export class BillPageComponent implements OnInit {
+export class BillPageComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  subscription: Subscription;
+
+  constructor(private billService: BillService) { }
 
   ngOnInit() {
+    this.subscription = Observable.combineLatest(
+      this.billService.getBill(),
+      this.billService.getCurrency()
+    ).subscribe((data: [Bill, any]) => {
+      console.log(data);
+    }) ;
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
