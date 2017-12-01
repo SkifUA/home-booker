11831778df6510ee5e2dac1ad580db39e13180b1
Valerii Kuzin <kuzin.valeriy@gmail.com>
2017-12-01 13:29:17 +0200

    Added HistoryPage component

--- src/app/system/system.module.ts
+++ src/app/system/system.module.ts
@@ -4,12 +4,16 @@ import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 import { SystemRoutingModule } from './system-routing.module';
 import { BillPageComponent } from './bill-page/bill-page.component';
+import { HistoryPageComponent } from './history-page/history-page.component';
 
 
 
 @NgModule({
   imports: [ CommonModule, SharedModule, SystemRoutingModule ],
-  declarations: [BillPageComponent]
+  declarations: [
+    BillPageComponent,
+    HistoryPageComponent
+  ]
 })
 
 export class SystemModule {}
