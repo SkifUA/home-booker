13b229e27e0f555a54f37af84059e873eae26b54
Valerii Kuzin <kuzin.valeriy@gmail.com>
2017-12-01 13:31:06 +0200

    Added PlaningPage component

--- src/app/system/system.module.ts
+++ src/app/system/system.module.ts
@@ -5,6 +5,7 @@ import { SharedModule } from '../shared/shared.module';
 import { SystemRoutingModule } from './system-routing.module';
 import { BillPageComponent } from './bill-page/bill-page.component';
 import { HistoryPageComponent } from './history-page/history-page.component';
+import { PlaningPageComponent } from './planing-page/planing-page.component';
 
 
 
@@ -12,7 +13,8 @@ import { HistoryPageComponent } from './history-page/history-page.component';
   imports: [ CommonModule, SharedModule, SystemRoutingModule ],
   declarations: [
     BillPageComponent,
-    HistoryPageComponent
+    HistoryPageComponent,
+    PlaningPageComponent
   ]
 })
 
