1d6c0bb1bd0114cfef616b4aed377f388760754f
Valerii Kuzin <kuzin.valeriy@gmail.com>
2017-12-02 19:37:24 +0200

    Added send new category to EditEvent

--- src/app/system/records-page/add-category/add-category.component.ts
+++ src/app/system/records-page/add-category/add-category.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 import { CategoriesService } from '../../shared/services/categories.service';
@@ -9,12 +9,13 @@ import { Category } from '../../shared/models/category.model';
   templateUrl: './add-category.component.html',
   styleUrls: ['./add-category.component.scss']
 })
-export class AddCategoryComponent implements OnInit {
+export class AddCategoryComponent {
+
+  @Output() onCategoryAdd = new EventEmitter<Category>();
 
   constructor(private categoriesService: CategoriesService) { }
 
-  ngOnInit() {
-  }
+
 
   onSubmit(form: NgForm) {
     let { name, capacity } = form.value;
@@ -28,6 +29,7 @@ export class AddCategoryComponent implements OnInit {
       .subscribe((category: Category) => {
           form.reset();
           form.form.patchValue({capcity: 1});
+          this.onCategoryAdd.emit(category);
       });
   }
 
