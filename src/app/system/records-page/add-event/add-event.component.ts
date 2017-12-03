2b54114ffda4bad6ddba56d1caffb88d982e3557
Valerii Kuzin <kuzin.valeriy@gmail.com>
2017-12-03 19:54:55 +0200

    Added message error for edit event and add unsubscribe

--- src/app/system/records-page/add-event/add-event.component.ts
+++ src/app/system/records-page/add-event/add-event.component.ts
@@ -1,19 +1,25 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import * as moment from 'moment';
+import { Subscription } from 'rxjs/Subscription';
 
 import { Category } from '../../shared/models/category.model';
 import { HBEvent } from '../../shared/models/event.model';
 import { EventsService } from '../../shared/services/events.service';
 import { BillService } from '../../shared/services/bill.service';
-import {Bill} from "../../shared/models/bill.model";
+import { Bill } from '../../shared/models/bill.model';
+import { Message } from '../../../shared/models/message.model';
+
 
 @Component({
   selector: 'hb-add-event',
   templateUrl: './add-event.component.html',
   styleUrls: ['./add-event.component.scss']
 })
-export class AddEventComponent implements OnInit {
+export class AddEventComponent implements OnInit, OnDestroy {
+
+  sub1: Subscription;
+  sub2: Subscription;
 
   @Input() categories: Category[] = [];
   types = [
@@ -21,16 +27,24 @@ export class AddEventComponent implements OnInit {
     {type: 'outcome', label: 'Outcome'},
   ];
 
+  message: Message;
+
   constructor(
     private eventsService: EventsService,
     private billService: BillService
   ) { }
 
   ngOnInit() {
+    this.message = new Message('danger', '');
+  }
+
+  private showMessage(text: string) {
+    this.message.text = text;
+    window.setTimeout(() => this.message.text = '', 5000);
   }
 
   onSubmit(form: NgForm) {
-    let { amount, description, category, type } = form.value;
+    let {amount, description, category, type} = form.value;
     if (amount < 0) {
       category *= -1;
     }
@@ -43,37 +57,38 @@ export class AddEventComponent implements OnInit {
       description
     );
 
-    this.billService.getBill()
+    this.sub1 = this.billService.getBill()
       .subscribe((bill: Bill) => {
         let value = 0;
 
         if (type === 'outcome') {
           if (bill.value < amount) {
-            // errors
+            this.showMessage(`Insufficient funds on the account. You are lacking ${amount - bill.value}`);
             return;
           } else {
             value = bill.value - amount;
           }
-
         } else {
           value = bill.value + amount;
         }
 
-        this.billService.updateBill({value, currency: bill.currency})
+        this.sub2 = this.billService.updateBill({value, currency: bill.currency})
           .mergeMap(() => this.eventsService.addEvent(event))
           .subscribe(() => {
             form.setValue({
               amount: 0,
-              description: '',
+              description: ' ',
               category: 1,
-              tupe: 'outcome'
+              type: 'outcome'
             });
           });
       });
 
-    // this.eventsService.addEvent(event);
-    //
-    // console.log(form.value);
+  }
+
+  ngOnDestroy() {
+    if (this.sub1)  this.sub1.unsubscribe();
+    if (this.sub2)  this.sub2.unsubscribe();
   }
 
 }
