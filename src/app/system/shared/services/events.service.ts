8b3dae57c6957b4a6728991545b7b20d04ef5550
Valerii Kuzin <kuzin.valeriy@gmail.com>
2017-12-03 17:40:38 +0200

    Added EventService

---
+++ src/app/system/shared/services/events.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+
+import { BaseApi } from '../../../shared/Core/base-api';
+import { HBEvent } from '../models/event.model';
+
+@Injectable()
+
+export class EventsService extends BaseApi {
+  constructor( public http: Http ) {
+    super(http);
+  }
+
+  addEvent(event: HBEvent): Observable<HBEvent> {
+    return this.post('events', event);
+  }
+}
