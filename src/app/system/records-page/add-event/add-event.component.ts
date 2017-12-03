235caaee002014b708c3a0d78922bf140d5c54ef
Valerii Kuzin <kuzin.valeriy@gmail.com>
2017-12-03 17:24:55 +0200

    Added EventModel

--- src/app/system/records-page/add-event/add-event.component.ts
+++ src/app/system/records-page/add-event/add-event.component.ts
@@ -1,7 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import * as moment from 'moment';
 
 import { Category } from '../../shared/models/category.model';
+import { HBEvent } from '../../shared/models/event.model';
 
 @Component({
   selector: 'hb-add-event',
@@ -22,6 +24,21 @@ export class AddEventComponent implements OnInit {
   }
 
   onSubmit(form: NgForm) {
+    let { amount, description, category, type } = form.value;
+    if (amount < 0) {
+      category *= -1;
+    }
+
+    const event = new HBEvent(
+      type,
+      amount,
+      +category,
+      moment().format('DD.MM.YYYY HH:mm:ss'),
+      description
+    );
+
+
+
     console.log(form.value);
   }
 
---
+++ src/app/system/shared/models/event.model.ts
@@ -0,0 +1,13 @@
+import * as moment from "moment";
+import _date = moment.unitOfTime._date;
+
+export class HBEvent {
+  constructor(
+    public type: string,
+    public amount: number,
+    public category: number,
+    public date: string,
+    public description: string,
+    public id?: number
+  ) {}
+}
