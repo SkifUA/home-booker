8fdb259e5c5edcf397c2397adcf0570007894f5f
Valerii Kuzin <kuzin.valeriy@gmail.com>
2017-12-06 00:09:56 +0200

    Added date for chart

--- src/app/system/history-page/history-chart/history-chart.component.ts
+++ src/app/system/history-page/history-chart/history-chart.component.ts
@@ -1,32 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'hb-history-chart',
   templateUrl: './history-chart.component.html',
   styleUrls: ['./history-chart.component.scss']
 })
-export class HistoryChartComponent implements OnInit {
+export class HistoryChartComponent {
 
-  data = [
-    {
-      "name": "Germany",
-      "value": 8940000
-    },
-    {
-      "name": "USA",
-      "value": 5000000
-    },
-    {
-      "name": "France",
-      "value": 7200000
-    }
-  ];
-  view: any[] = [545, 355];
-
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  @Input() data;
 }
--- src/app/system/history-page/history-page.component.ts
+++ src/app/system/history-page/history-page.component.ts
@@ -1,15 +1,67 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Subscription } from 'rxjs/Subscription';
+
+import { CategoriesService} from '../shared/services/categories.service';
+import { EventsService } from '../shared/services/events.service';
+import { Category } from '../shared/models/category.model';
+import { HBEvent } from '../shared/models/event.model';
+
 
 @Component({
   selector: 'hb-history-page',
   templateUrl: './history-page.component.html',
   styleUrls: ['./history-page.component.scss']
 })
-export class HistoryPageComponent implements OnInit {
+export class HistoryPageComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  constructor(
+    private categoriesService: CategoriesService,
+    private eventsService: EventsService
+  ) { }
+
+  isLoaded = false;
+  sub1: Subscription;
+
+  categories: Category[] = [];
+  events: HBEvent[] = [];
+
+  chartData = [];
 
   ngOnInit() {
+    this.sub1 = Observable.combineLatest(
+      this.categoriesService.getCategories(),
+      this.eventsService.getEvents()
+    ).subscribe((data: [Category[], HBEvent[]]) => {
+      this.categories = data[0];
+      this.events = data[1];
+
+      this.calculateChartData();
+
+      this.isLoaded = true;
+    } );
+  }
+
+  calculateChartData(): void {
+    this.chartData = [];
+
+    this.categories.forEach((c) => {
+      const catEvent = this.events.filter((e) => e.category === c.id && e.type === 'outcome');
+      this.chartData.push({
+        name: c.name,
+        value: catEvent.reduce((total, e) => {
+          total += e.amount;
+          return total;
+        }, 0)
+      });
+    });
+    console.log(this.chartData);
+  }
+
+  ngOnDestroy() {
+    if (this.sub1) {
+      this.sub1.unsubscribe();
+    }
   }
 
 }
