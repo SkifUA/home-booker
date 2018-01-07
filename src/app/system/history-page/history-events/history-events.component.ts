543112e90b1d3910f45aae488f761ade98c9d557
Valerii <kuzin.valeriy@gmail.com>
2018-01-08 00:20:07 +0200

    HistoryPage added list events

--- src/app/system/history-page/history-events/history-events.component.ts
+++ src/app/system/history-page/history-events/history-events.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+
+import { Category } from '../../shared/models/category.model';
+import { HBEvent } from '../../shared/models/event.model';
 
 @Component({
   selector: 'hb-history-events',
@@ -6,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./history-events.component.scss']
 })
 export class HistoryEventsComponent implements OnInit {
+  @Input() categories: Category[] = [];
+  @Input() events: HBEvent[] = [];
 
   constructor() { }
 
   ngOnInit() {
+    this.events.forEach((e) => {
+      e.catName = this.categories.find(c => c.id === e.category).name;
+    });
+  }
+
+  getEventClass(e: HBEvent) {
+    return {
+      'label': true,
+      'label-danger': e.type === 'outcome',
+      'label-success': e.type === 'income'
+    }
   }
 
 }
